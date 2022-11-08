5287d4e311746e425e39f08f927599d6f6a12c59
Rasel Parvej <RaselParvejABC@gmail.com>
2022-11-08 20:31:59 +0600

    MongoDB Atlas Gets Connected

---
+++ db/mongo-client.js
@@ -0,0 +1,15 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@mrp.nrrcot5.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+client.on("connectionCreated", () => {
+  console.log("Hello from Mongo!");
+});
+
+module.exports = client;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const client = require("./db/mongo-client");
 const app = express();
 const port = 5000;
 
