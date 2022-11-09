9d38536a4ff376d4f45458e60a443b339d3bc360
Rasel Parvej <RaselParvejABC@gmail.com>
2022-11-09 08:44:05 +0600

    Chrome not allows body with GET fixed

--- routers/services.js
+++ routers/services.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { mongoClient } = require("../db/mongo-client");
 const servicesCollection = mongoClient.db("math-mentor").collection("services");
 
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
   const options = {};
   if (req.body.limit) {
     options["limit"] = req.body.limit;
