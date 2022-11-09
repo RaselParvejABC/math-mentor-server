a36a9488e01cbe2e4ece369748d06b54c512345a
Rasel Parvej <RaselParvejABC@gmail.com>
2022-11-09 14:15:17 +0600

    POST /review done

--- routers/review.js
+++ routers/review.js
@@ -1,4 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const { getMongoClient } = require("../db/mongo-client");
 
+const mongoClient = getMongoClient();
+const reviewsCollection = mongoClient.db("math-mentor").collection("reviews");
+
+router.post("/", async (req, res) => {
+  const newReview = req.body;
+
+  try {
+    await reviewsCollection.insertOne(newReview);
+    res.json({ success: true });
+  } catch (error) {
+    res.json({ success: false });
+  }
+});
 module.exports = router;
